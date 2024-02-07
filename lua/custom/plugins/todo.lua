e0d0b957959834f644e921eb49b9f97aabe68edf
Mathias Djärv <mathias.djarv@gmail.com>
2024-02-07 13:16:44 +0100

    fix todo-comment not loading

--- lua/custom/plugins/todo.lua
+++ lua/custom/plugins/todo.lua
@@ -1,16 +1,17 @@
 return {
-	'folke/todo-comments.nvim',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	keys = {
+  'folke/todo-comments.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  event = 'VimEnter',
+  keys = {
     { '<leader>tT', '<cmd>TodoTrouble<cr>', desc = 'Todo' },
-	},
-	opts = {
-		highlight = {
-			keyword = 'bg',
-			pattern = [[.*<(KEYWORDS):?\s+.*]],
-		},
-		search = {
-			pattern = [[\b(KEYWORDS):?\b]],
-		},
-	}
+  },
+  opts = {
+    highlight = {
+      keyword = 'bg',
+      pattern = [[.*<(KEYWORDS):?\s+.*]],
+    },
+    search = {
+      pattern = [[\b(KEYWORDS):?\b]],
+    },
+  },
 }
