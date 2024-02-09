062b6fb4e6caa14e2bca82e1691b581495e8e141
Mathias Djärv <mathias.djarv@gmail.com>
2024-02-09 09:32:56 +0100

    fix move commands

--- lua/custom/plugins/autopairs.lua
+++
@@ -1,6 +0,0 @@
--- return { 'jiangmiao/auto-pairs' }
-return {
-	'windwp/nvim-autopairs',
-	event = 'InsertEnter',
-	opts = {},
-}
--- lua/custom/plugins/move.lua
+++ lua/custom/plugins/move.lua
@@ -1,19 +1,16 @@
 return {
-	'fedepujol/move.nvim',
-	config = function()
-		local opts = { noremap = true, silent = true }
-		-- Normal-mode commands
-		vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', opts)
-		vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', opts)
-		vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', opts)
-		vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', opts)
-		vim.keymap.set('n', '<leader>wf', ':MoveWord(1)<CR>', opts)
-		vim.keymap.set('n', '<leader>wb', ':MoveWord(-1)<CR>', opts)
-
-		-- Visual-mode commands
-		vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', opts)
-		vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', opts)
-		vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', opts)
-		vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', opts)
-	end
+  'fedepujol/move.nvim',
+  keys = {
+    { '<A-j>', ':MoveLine(1)<CR>' },
+    { '<A-k>', ':MoveLine(-1)<CR>' },
+    { '<A-h>', ':MoveHChar(-1)<CR>' },
+    { '<A-l>', ':MoveHChar(1)<CR>' },
+    { '<leader>wb', ':MoveWord(-1)<CR>' },
+    { '<leader>wf', ':MoveWord(1)<CR>' },
+    { '<A-j>', ':MoveBlock(1)<CR>', mode = 'v' },
+    { '<A-k>', ':MoveBlock(-1)<CR>', mode = 'v' },
+    { '<A-h>', ':MoveHBlock(-1)<CR>', mode = 'v' },
+    { '<A-l>', ':MoveHBlock(1)<CR>', mode = 'v' },
+  },
+  config = true,
 }
