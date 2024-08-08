6b2dd4d53442d39549bed419ea01a565d5bb4344
Mathias Djärv <mathias.djarv@gmail.com>
2024-08-08 14:14:23 +0200

    add lazygit

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -21,4 +21,20 @@ return {
     config = true,
   },
   'tpope/vim-rhubarb',
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = {
+      { '<leader>l', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
 }
